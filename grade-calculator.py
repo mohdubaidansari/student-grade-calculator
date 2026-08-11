1b18e2777229514793afac08554bc2e65792f920
mohdubaidansari <ua2109357@gmail.com>
2026-08-11 16:53:35 +0530

    Implement student grade calculator

--- grade-calculator.py
+++ grade-calculator.py
@@ -1,1 +1,18 @@
+print("student grade calculator")
+marks=float(input("enter your marks (out of 100):))
+
+if marks>=90:
+grade ="A+"
+elif marks>=80:
+grade ="A"
+elif marks>=70:
+grade="B"
+elif marks>=60:
+grade="C"
+elif marks>=50:
+grade="D"
+else:
+grade="F"
+
+print("Your grade is",grade)
 
